2f50d1ddbc6a7e6ec925d008e913faaa174d0a00
woorise <woorisecom@gmail.com>
2022-03-20 15:20:48 +0200

    Better load inline scripts

--- woorise.php
+++ woorise.php
@@ -4,7 +4,7 @@
  * Plugin Name: Woorise
  * Plugin URI: https://woorise.com/
  * Description: Create viral giveaways & contests, engaging forms, landing pages, surveys, quizzes, collect payments and more.
- * Version: 1.3.4
+ * Version: 1.3.5
  * Requires at least: 5.6
  * Author: Woorise
  * Author URI: https://woorise.com/
@@ -19,20 +19,7 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/class-woorise-embed.php' )
 
 function woorise_embed_block_init() {
 	register_block_type_from_metadata( __DIR__ . '/src', [
-		'render_callback' => 'woorise_embed_block_handler',
+		'render_callback' => [ new Woorise_Embed(), 'woorise_embed_block_handler' ]
 	] );
 }
 add_action( 'init', 'woorise_embed_block_init' );
-
-function woorise_embed_block_handler( $atts ) {
-
-  $woorise_embed = new Woorise_Embed();
-
-  $class = isset( $atts['className'] ) ? 'wp-block-woorise ' . $atts['className'] : 'wp-block-woorise';
-
-  return sprintf(
-    '<div class="%1$s">%2$s</div>',
-    esc_attr( $class ),
-    $woorise_embed->get_embed( $atts['url'] )
-  );
-}
